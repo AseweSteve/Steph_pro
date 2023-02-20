255fbfb6dbe05618e941ab198d587e0747c403ef
Asewe Creatives <123335187+AseweSteve@users.noreply.github.com>
2023-02-20 03:18:40 +0300

    huge commit

--- public/script.js
+++ public/script.js
@@ -4,3 +4,10 @@ const navLinks = document.querySelector('.nav-links');
 menu.addEventListener('click', () => {
   navLinks.classList.toggle('hide');
 });
+
+const checkbox = document.querySelector("#checkbox");
+
+checkbox.addEventListener("change", () => {
+  // Toggle website theme
+  document.body.classList.toggle("dark");
+});
